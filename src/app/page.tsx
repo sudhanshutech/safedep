4895782800dcc5e9a96b485bc21a4627975b13a6
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2025-02-11 13:47:39 +0530

    add shadow support for client api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
 import PackageDashboard from "./dashboard";
 import sampleData from "../sampleData/sample.json";
 
+/* This component is the fetching data from the insights-client using server side props */
+// import { getPackageVersionInsightData } from "../../insights-client";
+
+// export async function getServerSideProps() {
+//   try {
+//     const data = await getPackageVersionInsightData();
+//     console.log("Received insights in page:", data);
+//     return {
+//       props: {
+//         data, // Pass the fetched data as props to the page
+//       },
+//     };
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     return {
+//       props: {
+//         error: "Failed to fetch data", // Handle errors
+//       },
+//     };
+//   }
+// }
+
 export default function Page() {
   return <PackageDashboard insights={sampleData} />;
 }
